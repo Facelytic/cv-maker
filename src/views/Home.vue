30496c8c658662f57d99fa14e0b40065cfebc26a
erwinwahyura <erwinwahyuramadhan@gmail.com>
2018-07-26 04:48:33 +0700

    face

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <Header></Header>
+    <div class="mid">
+      <Profile></Profile>
+    </div>
+   <div class="body">
+      <Content></Content>
+   </div>
+   <div class="body">
+      <Content></Content>
+   </div>
+   <div class="body">
+      <Content></Content>
+   </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
+import Header from "@/components/Header"
+import Profile from "@/components/Profile"
+import Content from "@/components/Content"
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld,
-  },
-};
+    Header,
+    Profile,
+    Content
+  }
+}
 </script>
+
+<style lang="less" scoped>
+#home {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-wrap: wrap;
+  background-color: #dedede;
+  border: 1px solid #dedede;
+}
+.mid {
+  display: flex;
+  width: 100%;
+}
+.body {
+  margin: 50px;
+  display: flex;
+  width: 100%;
+}
+</style>
+
